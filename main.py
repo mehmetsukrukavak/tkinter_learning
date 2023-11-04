8b469ddb6f705285cd94180c9f23f6f8271e44da
Mehmet Sukru Kavak <mehmetsukru.kavak07@gmail.com>
2023-11-04 09:23:09 +0300

    grid example

--- main.py
+++ main.py
@@ -11,24 +11,25 @@ screen_x = window.winfo_width()
 myLabel = tkinter.Label(text="This is a Label",font=("Arial", 30, "bold"))
 myLabel.config(bg="black",fg="white")
 #myLabel.pack()
-myLabel.place(x=0,y=0)
+#myLabel.place(x=0,y=0)
+myLabel.grid(row=0,column=0)
 
 #entry
 myEntry = tkinter.Entry(width=20)
 #myEntry.pack()
-myEntry.place( x=100,y=50)
-
+#myEntry.place( x=100,y=50)
+myEntry.grid(row=0,column=3)
 #button
 def click_button():
     print("Button clicked")
     userInput = myEntry.get()
     print(userInput)
 myButton = tkinter.Button(text="This is a Button",command=click_button)
-myButton.place(x=screen_x / 2 -65,y=screen_y / 2 -14)
-myButton.update()
-
+#myButton.place(x=screen_x / 2 -65,y=screen_y / 2 -14)
+#myButton.update()
 #print(myButton.winfo_width())
 #print(myButton.winfo_height())
+myButton.grid(row=1,column=2)
 
 #myButton.pack()
 
