5868470f21ed213387b832882ebc3cb37e90e930
Mehmet Sukru Kavak <mehmetsukru.kavak07@gmail.com>
2023-11-04 09:18:58 +0300

    place example

--- main.py
+++ main.py
@@ -4,15 +4,19 @@ window = tkinter.Tk()
 window.title("Python Tkinter Example")
 window.minsize(450, 300)
 
-
+window.update()
+screen_y = window.winfo_height()
+screen_x = window.winfo_width()
 #label
 myLabel = tkinter.Label(text="This is a Label",font=("Arial", 30, "bold"))
 myLabel.config(bg="black",fg="white")
-myLabel.pack()
+#myLabel.pack()
+myLabel.place(x=0,y=0)
 
 #entry
 myEntry = tkinter.Entry(width=20)
-myEntry.pack()
+#myEntry.pack()
+myEntry.place( x=100,y=50)
 
 #button
 def click_button():
@@ -20,6 +24,12 @@ def click_button():
     userInput = myEntry.get()
     print(userInput)
 myButton = tkinter.Button(text="This is a Button",command=click_button)
-myButton.pack()
+myButton.place(x=screen_x / 2 -65,y=screen_y / 2 -14)
+myButton.update()
+
+#print(myButton.winfo_width())
+#print(myButton.winfo_height())
+
+#myButton.pack()
 
 window.mainloop()
