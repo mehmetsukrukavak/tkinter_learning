9848e979f6f18af1b8891b1de4de8ebd1d0371b9
Mehmet Sukru Kavak <mehmetsukru.kavak07@gmail.com>
2023-11-04 08:59:48 +0300

    widgets(label, entry, button)

--- main.py
+++ main.py
@@ -4,4 +4,22 @@ window = tkinter.Tk()
 window.title("Python Tkinter Example")
 window.minsize(450, 300)
 
+
+#label
+myLabel = tkinter.Label(text="This is a Label",font=("Arial", 30, "bold"))
+myLabel.config(bg="black",fg="white")
+myLabel.pack()
+
+#entry
+myEntry = tkinter.Entry(width=20)
+myEntry.pack()
+
+#button
+def click_button():
+    print("Button clicked")
+    userInput = myEntry.get()
+    print(userInput)
+myButton = tkinter.Button(text="This is a Button",command=click_button)
+myButton.pack()
+
 window.mainloop()
